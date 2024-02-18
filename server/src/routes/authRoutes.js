83b7637ffed89aef287a58fd32e5fc3325ff6d2e
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-02-18 12:36:45 +0530

    video player  V.1

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const {
+  SignIn,
+  SignUp,
+  Profile,
+  approveCreator,
+  disapproveCreator,
+} = require("../controller/authController");
+const {
+  SIGNUP_VALIDATOR,
+  LOGIN_VALIDATOR,
+  isRequestValidated,
+} = require("../config/validator");
+const {
+  requiredSignin,
+  adminMiddleware,
+} = require("../common/middleware");
+
+// Define your API endpoints here
+router.post("/signup", SIGNUP_VALIDATOR, isRequestValidated, SignUp);
+router.post("/signin", LOGIN_VALIDATOR, isRequestValidated, SignIn);
+router.post("/profile", requiredSignin, Profile);
+router.get(
+  "/approve-creator/:createId",
+  requiredSignin,
+  adminMiddleware,
+  approveCreator
+);
+router.get(
+  "/disapprove-creator/:createId",
+  requiredSignin,
+  adminMiddleware,
+  disapproveCreator
+);
+
+module.exports = router;
---
+++ server/src/server.js
@@ -0,0 +1,55 @@
+/* Author: SANJU BODRA */
+
+const express = require("express");
+const cors = require('cors');
+const { connectDB } = require("./config/db");
+
+const app = express();
+
+const authRoutes = require("./routes/authRoutes");
+const creatorRoutes = require("./routes/creatorRoutes");
+const walletRoutes = require("./routes/walletRoutes");
+const subscriptionRoutes = require("./routes/subscriptionRoutes");
+
+async function main() {
+  // Environment variables
+  require("dotenv").config();
+
+  // Mibbleware configuration
+  app.use(cors())
+  app.use(express.json());
+
+  app.use(express.urlencoded({
+    extended: false
+  }))
+
+  // Connect to Database
+  try {
+    await connectDB();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Failed to connect to the database:', error.message);
+    process.exit(1); // Exit the process if unable to connect to the database
+  }
+}
+
+main();
+
+
+
+
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Hello World",
+  });
+});
+
+//Auth Routes
+app.use("/auth/v1", [authRoutes]);
+
+//Routes
+app.use("/api/v1", [creatorRoutes, walletRoutes, subscriptionRoutes]);
+
+app.listen(3000 || process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
